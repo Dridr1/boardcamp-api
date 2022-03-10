ccebb4774c66322b0e4d950856110fd6d2817b6b
leo <celsolnv27@gmail.com>
2022-03-10 09:18:46 -0300

    Preparando revisão de código

--- src/middlewares/validateNewCategorie.js
+++
@@ -1,9 +0,0 @@
-export const validateNewCategorie = (schema) => {
-    return (req, res, next) => {
-        const validation = schema.validate(req.body);
-        if(validation.error) {
-            return res.sendStatus(409);
-        }
-        next();
-    }
-}
